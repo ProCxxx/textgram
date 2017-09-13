12053f09ae95c802512571292e698adc5a4be9c0
procxxx <proxturt@gmail.com>
2017-09-13 13:19:46 +0200

    Initial

--- php.php
+++ php.php
@@ -1,9 +1,36 @@
 <?php
 
+if(strlen($_COOKIE['username'])>4){
+  setcookie("username","xyza",time(),"/");
+  header("Location: index.php");
+}
+
 $name=$_POST['name'];
 $mail=$_POST['mail'];
 $msg=$_POST['msg'];
 $ret=$_POST['ret'];
 
+
+
+$servername = "localhost";
+$username = "id1115136_proxy12";
+$password = "cartown12";
+$dbname = "id1115136_chatuser";
+
+
+$conn = mysqli_connect($servername, $username, $password,$dbname);
+
+
+if (!$conn) {
+    die("Connection failed!");
+}
+
+
+$sql = "INSERT INTO `contact`(`id`, `name`, `mail`, `msg`) VALUES (,$name,$mail,$msg);
+$result = mysqli_query($conn, $sql);
+
+mysqli_close($conn);
+
+  header("Location: $ret");
 
  ?>
