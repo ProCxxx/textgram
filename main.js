65819f1ba267cd0f4e082ebc906f0342efb6150f
procxxx <proxturt@gmail.com>
2017-09-14 02:43:21 +0200

    Initial

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 //Pr0xY
 
+function chang()
+{
+  var x = $("#mes").val();
+  $("#rem").text( x.length );
+}
+
+function kedow()
+{
+  var x = $("#mes").val();
+  $("#rem").text( x.length );
+}
+
 //Pr0xY
